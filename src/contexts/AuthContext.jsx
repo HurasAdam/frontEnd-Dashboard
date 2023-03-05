6b54164f432927ade79891f6fca59c392eea4143
HurasAdam <huradam@wp.pl>
2023-03-05 02:38:27 +0100

    fix updating authContext based on local storage

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,9 +1,17 @@
-import { createContext, useReducer } from "react"
+import { createContext, useEffect, useReducer } from "react"
 import { authReducer } from "../reducers/authReducer"
 
 export const AuthContext= createContext()
 export const AuthContextProvider=({children})=>{
 
+    useEffect(()=>{
+
+const user= JSON.parse(localStorage.getItem('user'))
+
+if(user){
+    dispatch({type:"LOGIN",payload:user})
+}
+    },[])
     
 
 const [state,dispatch]=useReducer(authReducer,{user:null})

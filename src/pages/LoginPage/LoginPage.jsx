f876f0423ccc2191960994da5c01c1502424be2d
HurasAdam <huradam@wp.pl>
2023-12-09 21:36:21 +0100

    Refactor LoginPage component and styles: -Move login form input handling logic to FormInput component, Each input field in the login form is now managed by the reusable FormInput component, making the code more modular and maintainable.

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,61 +1,109 @@
-import './loginPage.css';
-import { useContext, useState,use, useEffect } from 'react';
-import { AuthContext } from '../../contexts/AuthContext';
-import { settLocalStorage } from '../../utils/SettlocalStorage';
-import { ThemeContext } from '../../contexts/ThemeContext'
-export const LoginPage=()=>{
-const {dispatch,user}=useContext(AuthContext)
-const {theme}=useContext(ThemeContext)
-const [email,setEmail]=useState('')
-const [password,setPassword]=useState('')
-const [error,setError]=useState(null)
+import "./loginPage.css";
+import { useContext, useState, use, useEffect } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+import { settLocalStorage } from "../../utils/SettlocalStorage";
+import { ThemeContext } from "../../contexts/ThemeContext";
+import { inputs } from "../../utils/inputs";
+import { FormInput } from "../../components/formInput/FormInput";
+export const LoginPage = () => {
+  const { dispatch, user } = useContext(AuthContext);
+  const { theme } = useContext(ThemeContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
 
+  const onHandleChange = (e, input) => {
+    setValues((prev) => {
+      return { ...prev, [input.name]: e.target.value };
+    });
+  };
 
-const handleClick=async(e)=>{
-e.preventDefault()
+  const handleLogin = async (e) => {
+    e.preventDefault();
 
-const response =await fetch('http://127.0.0.1:3000/api/user/login',{
-    method:"POST",
-    headers:{"Content-Type":"application/json"},
-    body:JSON.stringify({email,password})
-})
+    const response = await fetch("http://127.0.0.1:3000/api/user/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(values),
+    });
 
-const data= await response.json()
+    const data = await response.json();
 
-if(!response.ok){
-    setError(data)
-    setEmail('')
-    setPassword('')
-    console.log(error)
-}
-if(response.ok){
-    setEmail('')
-    setPassword('')
-    
-    console.log('succes')
-    settLocalStorage(data)
-dispatch({type:"LOGIN",payload:data})
+    if (!response.ok) {
+      setError(data);
+      setEmail("");
+      setPassword("");
+      console.log(error);
+    }
+    if (response.ok) {
+      setEmail("");
+      setPassword("");
 
-}
+      console.log("succes");
+      settLocalStorage(data);
+      dispatch({ type: "LOGIN", payload: data });
+    }
+  };
 
+  const handleClick = async (e) => {
+    e.preventDefault();
 
+    const response = await fetch("http://127.0.0.1:3000/api/user/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
 
+    const data = await response.json();
 
+    if (!response.ok) {
+      setError(data);
+      setEmail("");
+      setPassword("");
+      console.log(error);
+    }
+    if (response.ok) {
+      setEmail("");
+      setPassword("");
 
+      console.log("succes");
+      settLocalStorage(data);
+      dispatch({ type: "LOGIN", payload: data });
+    }
+  };
 
-}
+  return (
+    <div className="loginPage" id={theme.mode}>
+      {/* <form className="loginPage-form" action="">
+        <label htmlFor="">Email</label>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <label htmlFor="">Password</label>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        <button onClick={handleClick}>Log in</button>
+        {error && <div className="error">{error}</div>}
+      </form> */}
 
-    return(
-        <div className="loginPage" id={theme.mode}>
-       <form className='loginPage-form' action="">
-            <label htmlFor="">Email</label>
-            <input type="email" onChange={(e)=>setEmail(e.target.value)} value={email} />
-            <label htmlFor="">Password</label>
-            <input type="password"onChange={(e)=>setPassword(e.target.value)} value={password} />
-           <button onClick={handleClick}>Log in</button>
-         {error&&<div className='error'>{error}</div>}
-           </form>
-           </div>
-    )
-}
+      <form className="loginPage-form">
+        <h1>Log in</h1>
+        {inputs.map((input) => {
+          return <FormInput {...input} onHandleChange={onHandleChange} />;
+        })}
+        <button onClick={(e) => handleLogin(e)}>LOG IN</button>
+        {error&&<div className='error'>{error}</div>}
+      </form>
+    </div>
+  );
+};

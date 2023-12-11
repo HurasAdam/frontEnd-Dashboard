ca30f487688d09d6a4c89b385050fc19edbe48e8
HurasAdam <huradam@wp.pl>
2023-12-11 01:06:59 +0100

    Refactor NewProject component and styles: now dynamically generate form using newProjectFormConfig function.

--- src/pages/newProject/NewProject.jsx
+++ src/pages/newProject/NewProject.jsx
@@ -5,66 +5,67 @@ import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
 import { useContext } from "react";
 import Select from "react-select";
-import { ThemeContext } from '../../contexts/ThemeContext'
-import {createProject} from "../../features/projectApi/projectApi"
+import { ThemeContext } from "../../contexts/ThemeContext";
+import { createProject } from "../../features/projectApi/projectApi";
 import { getUsers } from "../../features/userApi/userApi";
 import { useMutation, useQuery } from "react-query";
-import {mutationHandler} from "../../shared/mutationHandler"
+import { mutationHandler } from "../../shared/mutationHandler";
+import { newProjectFormConfig } from "../../utils/newProjectFormConfig";
+import { FormInput } from "../../components/formInput/FormInput";
 
 export const NewProject = () => {
-  const [title, setTitle] = useState('');
-  const [description, setDescription]=useState('');
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
   const [contributors, setContributors] = useState([]);
-  
-  const {isLoading,isError,error,data:users,refetch}=useQuery(["people"],()=>getUsers(),{
-    refetchOnWindowFocus:false,
-    enabled:false
-  })
+
+  const [values, setValues] = useState({
+    title: "",
+    description: "",
+    contributors: [],
+  });
+
+  const {
+    isLoading,
+    isError,
+    error,
+    data: users,
+    refetch,
+  } = useQuery(["people"], () => getUsers(), {
+    refetchOnWindowFocus: false,
+    enabled: false,
+  });
 
   // const createMutation = useMutation(createProject,{
   //   onSuccess:(data)=>{
   //     navigate(`/projects`)
   //   }
   // })
-  
 
+  const handleCreateProject = (
+    e,
+    { title, description, contributors },
+    mutation
+  ) => {
+    e.preventDefault();
 
+    mutation.mutate({ title, description, contributors });
+  };
 
-  const handleCreateProject=(e,{title,description,contributors},mutation)=>{
-e.preventDefault();
-
-
-mutation.mutate({title,description,contributors})
-
-  }
-
-const createProjectMutation = mutationHandler(createProject,(data)=>{
-  navigate('/projects')
-})
-
+  const createProjectMutation = mutationHandler(createProject, (data) => {
+    navigate("/projects");
+  });
 
   const customStyles = {
     control: (provided, state) => ({
       ...provided,
-      backgroundColor:'rgb(238, 240, 240)',
-     
+      backgroundColor: "rgb(238, 240, 240)",
     }),
     option: (provided, state) => ({
       ...provided,
-      backgroundColor:  'rgb(238, 240, 240)' ,
+      backgroundColor: "rgb(238, 240, 240)",
     }),
   };
 
-
-
-
-
-
-
-  const handleChange = (selectedOptions) => {
-const selectedUsers=selectedOptions.map((option)=>option._id)
-    setContributors(selectedUsers)
-      };
 
 
 
@@ -73,44 +74,22 @@ const selectedUsers=selectedOptions.map((option)=>option._id)
   const [selecedtUsers, setSelecedtUsers] = useState([]);
   const navigate = useNavigate();
   const { user, dispatch } = useContext(AuthContext);
-  const {theme}=useContext(ThemeContext)
+  const { theme } = useContext(ThemeContext);
 
-  //handle new project inputs
-  // const handleNewProject = (e, prop) => {
-  //   const value = e.target.value;
-  //   newProject[prop] = value;
-  //   console.log(newProject);
-  // };
+  // const [values,setValues]=useState({
+  //   options:users&&users,
 
+  // })
 
+  const inputs = newProjectFormConfig({ users: users, refetch,setValues });
 
-
-  // useEffect(() => {
-  //   getUserList();
-  // }, []);
-
-  //get list of users 
-  // const getUserList = async () => {
-  //   const response = await fetch("http://127.0.0.1:3000/api/user/",{
-  //     headers:{'Authorization': `Bearer ${user.token}`},
-  //   });
-
-  //   const json = await response.json();
-
-  //   const arr = json.map((user) => {
-  //     return { value: user._id, label: user.email };
-  //   });
-
-  //   setUserList(arr);
-  // };
-
-  //Add new project 
+  //Add new project
   const handleAddProject = async () => {
     const response = await fetch("http://127.0.0.1:3000/api/projects", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        'Authorization': `Bearer ${user.accessToken}`
+        Authorization: `Bearer ${user.accessToken}`,
       },
       body: JSON.stringify(objectToSend),
     });
@@ -119,76 +98,18 @@ const selectedUsers=selectedOptions.map((option)=>option._id)
     }
   };
 
-
-
   return (
     <div className="newProject" id={theme.mode}>
-        <div className="newProjectTop">
-          <span>New Project</span>
-         
-        </div>
-      <div className="newProjectConent" id={theme.mode}>
-      <div className="newProjectContentTopButton">
-      <Link to="/Projects">
-            <button>X</button>
-          </Link>
-      </div>
-        <div className="newProjectBottom">
-          <div className="newProjectItem">
-            <label className="newProjectItemLabel" htmlFor="">
-              Project Title
-            </label>
-            <input type="text" onChange={(e)=>setTitle(e.target.value)} />
-          </div>
-
-          <div className="newProjectItemDescripion">
-            <label className="newProjectItemLabel" htmlFor="">
-              Description
-            </label>
-            <textarea
-              name=""
-              id=""
-              rows={25}
-              onChange={(e) =>setDescription(e.target.value)}
-            ></textarea>
-          </div>
-          <div className="newProjectItem">
-            <label className="newProjectItemLabel" htmlFor="">
-              Asign Members
-            </label>
-            <Select 
-             
-            className="newProjectMemberSelect"
-              options={users&&users}
-              isMulti
-              isSearchable
-              styles={customStyles}
-              onChange={handleChange}
-              onFocus={refetch}
-              getOptionLabel={(option) =>
-                `${option.name} ${option.surname}`
-              }
-              getOptionValue={(option) => option._id}
-          
-            ></Select>
-          </div>
-        </div>
-        <div className="newProjectAction">
-          <button
-          
-          onClick={(e)=>handleCreateProject(e,{title,description,contributors},createProjectMutation)}
-          className="newProjectSave">
-            Save
-          </button>
-          <button
-            className="newProjectCancel"
-            onClick={() => navigate("/Projects")}
-          >
-            Cancel
-          </button>
-        </div>
+      <div className="newProjectTop">
+        <span>New Project</span>
       </div>
    
+
+      {inputs.map((input) => {
+        return <FormInput {...input}
+        className="newProject-form"
+        />;
+      })}
     </div>
   );
 };

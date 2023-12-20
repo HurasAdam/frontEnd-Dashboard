7fd99496a5bb022156157948070f8d260c611556
HurasAdam <huradam@wp.pl>
2023-12-20 05:27:16 +0100

    Refactor: rework Donut component and introduce chartConfig function: - Moved the state from Donut component to useState hook for better readability and maintainability. - Introduced the chartConfig function in a separate file to handle chart configuration. - Adjusted the usage of the state and labels in the Donut component accordingly. - Improved code structure and organization for clarity.

--- src/components/donutChart/Donut.jsx
+++ src/components/donutChart/Donut.jsx
@@ -3,26 +3,15 @@ import Chart from "react-apexcharts";
 import "../../components/donutChart/donut.css";
 import { useState } from "react";
 import missingData from "../../../public/img/empty.png";
+import { chartConfig } from "../../utils/chartConfig";
 
-export const Donut = ({ data, theme }) => {
+export const Donut = ({data, theme }) => {
 
-
-  const getLegendColor = (themeMode) => {
-    switch (themeMode) {
-      case 'light':
-        return 'black';
-      case 'dark':
-        return 'white';
-      case 'purple':
-        return 'orange';
-        case'green':
-        return'white';
-      default:
-        return 'black';
-    }
-  }
-
-
+  const [state, setState] = useState({
+    series: [],
+    labels:[],
+  });
+  const donutChartConfig = chartConfig("donut-dashboard",state.labels,theme);
 
   useEffect(() => {
     if (data) {
@@ -39,85 +28,15 @@ export const Donut = ({ data, theme }) => {
       setState({
         ...state,
         series: arrayOfValues,
-        options: {
-          ...state.options,
-          labels: arrayOfKeys,
-          legend: {
-            ...state.options.legend,
-            labels: {
-              ...state.options.legend.labels,
-              colors: getLegendColor(theme.mode)
-            },
-          },
-          dataLabels: {
-            ...state.options.dataLabels,
-            style: {
-              ...state.options.dataLabels.style,
-              colors: theme.mode === "dark" ? ["white"] : ["rgb(212, 81, 81);"],
-            },
-            background: {
-              ...state.options.dataLabels.background,
-              enabled: theme.mode === "light" ? true : false,
-            },
-          },
-        },
+        labels:arrayOfKeys
       });
     }
   }, [data]);
 
-  const [state, setState] = useState({
-    options: {
-      dataLabels: {
-        enabled: true,
-
-        style: {
-          colors: [],
-        },
-        background: {
-          borderColor: "",
-        },
-      },
-      legend: {
-        position: "bottom",
-        fontSize: "13px",
-        labels: {
-          colors: "",
-        },
-      },
-      labels: [],
-      responsive: [
-        {
-          breakpoint: 480,
-          options: {
-            chart: {
-              width: 400,
-            },
-            legend: {
-              position: "bottom",
-            },
-          },
-        },
-        {
-          breakpoint: 390,
-          options: {
-            chart: {
-              width: 250,
-            },
-            legend: {
-              position: "bottom",
-            },
-          },
-        },
-      ],
-      maintainAspectRatio: false,
-    },
-    series: [],
-  });
-
   return (
     <div className={`donut ${state.series.length>0?'':'hidden'}`}>
       {state.series.length > 0 ? (
-        <Chart options={state.options} series={state.series} type="donut" />
+        <Chart options={donutChartConfig} series={state.series} type="donut" />
       ) : (
         <div className="missingDataPlaceholder">
           <div className="missingaDataImgWrapper">

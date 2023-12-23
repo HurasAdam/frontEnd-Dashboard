e405dfdd73d05f67904d68bcfe600ad07a360911
HurasAdam <huradam@wp.pl>
2023-12-23 02:54:33 +0100

    Rework: Extend project schema with addActivity method.

--- server/db/models/project.js
+++ server/db/models/project.js
@@ -1,6 +1,41 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
+const daySchema= new Schema({
+  date:{
+    type:Date,
+    default:function(){
+      return new Date()
+    }
+  },
+  contributions:{
+    type:Number,
+    default:0
+  },
+  day:{
+    type:Number
+  }
+  
+})
+
+const monthSchema = new Schema({
+  month:{
+    type:Number,
+    required:true
+  },
+  days:[daySchema]
+})
+
+const contributorSchema = new Schema({
+  _id:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  activity:[monthSchema]
+})
+
+
 const projectSchema = new Schema({
   title: {
     type: String,
@@ -10,29 +45,7 @@ const projectSchema = new Schema({
     type: String,
     required: true,
   },
-  contributors: [
-    {
-      _id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-      },
-      activity: [
-        {
-          date: {
-            type: Date,
-            default: function () {
-              return new Date();
-            },
-          },
-          contributions: {
-            type: Number,
-            default: 0,
-          },
-        },
-      ],
-    },
-  ],
+  contributors: [contributorSchema],
 
   projectLeader: {
     type: mongoose.Schema.Types.ObjectId,
@@ -71,6 +84,70 @@ const projectSchema = new Schema({
     default: Date.now(),
   },
 });
+
+projectSchema.methods.addActivity = async function (contributions,contributorId) {
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth() + 1; // Miesiące są indeksowane od zera
+  const currentYear = currentDate.getFullYear();
+
+  // Sprawdzamy, czy istnieje już obiekt dla bieżącego miesiąca
+const getDaysInMonth = (year,month)=>{
+ return  new Date(year, month + 1, 0).getDate()
+}
+
+const days = getDaysInMonth(currentYear,currentMonth)
+const daysObject= Array.from({length:days},(_,index)=>({
+  day:index+1,
+  contributions:0
+}))
+
+
+
+const contributorAcitivty= contributorId.activity
+
+
+
+const index = contributorAcitivty.findIndex((obj) => obj.month === currentMonth);
+
+if (index !== -1) {
+
+  console.log("Index obiektu:", index);
+console.log(contributorAcitivty[index])
+}
+
+const monthObj = {
+month:currentMonth,
+days:daysObject
+}
+contributorAcitivty.push
+(monthObj)
+
+
+
+  // if (currentMonthObject) {
+  //   // Jeśli istnieje, zwiększamy istniejącą aktywność
+  //   currentMonthObject.days.forEach((day) => {
+  //     day.contributions += contributions;
+  //   });
+  // } else {
+  //   // Jeśli nie istnieje, tworzymy nowy obiekt miesiąca
+  //   const daysInMonth = new Date(currentYear, currentMonth, 0).getDate();
+  //   const newMonthObject = {
+  //     month: currentMonth,
+  //     days: Array.from({ length: daysInMonth }, () => ({
+  //       date: new Date(currentYear, currentMonth - 1, day + 1),
+  //       contributions: contributions,
+  //     })),
+  //   };
+
+  //   this.activity.push(newMonthObject);
+  // }
+
+  await this.save();
+};
+
+
+
 
 const model = mongoose.model("Projects", projectSchema);
 

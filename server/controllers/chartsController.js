4bc7b95e38e416c42a2b26a98599f017f3c506cc
HurasAdam <huradam@wp.pl>
2023-12-24 01:05:04 +0100

    Refactor: Improve getMemberContributions endpoint for better readability, Changed variable name for better clarity.

--- server/controllers/chartsController.js
+++ server/controllers/chartsController.js
@@ -4,16 +4,30 @@ const Project = require("../db/models/project")
 const getMemberContributions=async(req,res)=>{
 
     const {project,contributor} = req.query
-  
+    // Project.aggregate(
+    //     [
+    //        {$match:{
+    //         "contributors._id":mongoose.Types.ObjectId(userId)
+    //        }},
+    //        {
+    //         $unwind:"$contributors"
+    //        },
+    //        {
+    //         $match:{
+    //             "contributors._id"
+    //         }
+    //        }
+    //     ]
+    // )
 
  
-const xd = await Project.findOne({_id:project})
+const currentProject = await Project.findOne({_id:project})
 
+currentProject.grpBy(currentProject.contributors[0].activity)
 
-const currentContributors= xd.contributors
-const filter = currentContributors.filter((cont)=>cont._id.toString()===contributor)
-return res.status(200).json(filter)
-   
+const contributors= currentProject.contributors
+const currentContributor = contributors.filter((cont)=>cont._id.toString()===contributor)
+return res.status(200).json(currentContributor)
 
 }
 

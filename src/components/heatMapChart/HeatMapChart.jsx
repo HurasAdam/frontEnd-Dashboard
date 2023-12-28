7aa2ad70ee1d4f016dde34bb3d53edf1633a073e
HurasAdam <huradam@wp.pl>
2023-12-28 02:10:47 +0100

    Refactor HeatMapChart component

--- src/components/heatMapChart/HeatMapChart.jsx
+++ src/components/heatMapChart/HeatMapChart.jsx
@@ -1,67 +1,8 @@
 import { useState } from "react";
-import "./heatMapChart.css"
+import "./heatMapChart.css";
 import ReactApexChart from "react-apexcharts";
-export const HeatMapChart = () => {
+export const HeatMapChart = ({ setSelectionOptions,result }) => {
   const [month, setMonth] = useState("");
-  function getDaysInMonth(month) {
-    let adjustedMonth;
-    switch (month) {
-      case "Januray":
-        adjustedMonth = 0;
-        break;
-      case "February":
-        adjustedMonth = 1;
-        break;
-      case "March":
-        adjustedMonth = 2;
-        break;
-      case "April":
-        adjustedMonth = 3;
-        break;
-      case "May":
-        adjustedMonth = 4;
-        break;
-      case "June":
-        adjustedMonth = 5;
-        break;
-      case "July":
-        adjustedMonth = 6;
-        break;
-      case "August":
-        adjustedMonth = 7;
-        break;
-      case "September":
-        adjustedMonth = 8;
-        break;
-      case "October":
-        adjustedMonth = 9;
-        break;
-      case "November":
-        adjustedMonth = 10;
-        break;
-      case "December":
-        adjustedMonth = 11;
-        break;
-      default:
-        adjustedMonth = 0;
-    }
-    // Uwaga: Numer miesiąca zaczyna się od 0 (styczeń) do 11 (grudzień)
-    return new Date(2023, adjustedMonth + 1, 0).getDate();
-  }
-
-  function generateHeatmapData(year, month) {
-    const daysInMonth = getDaysInMonth(year, month);
-    const series = Array.from({ length: daysInMonth }, (_, day) => ({
-      name: `Day ${day + 1}`,
-      data: Array.from({ length: 7 }, (_, weekday) => ({
-        x: weekday,
-        y: Math.floor(Math.random() * 100),
-      })),
-    }));
-    return series;
-  }
-  const checkDays = generateHeatmapData(month);
-
 
 
   const [state, setState] = useState({
@@ -71,8 +12,7 @@ export const HeatMapChart = () => {
         data: [
           {
             x: "Week 1",
-            y:3
-           
+            y: 3,
           },
           {
             x: "Week 2",
@@ -86,9 +26,7 @@ export const HeatMapChart = () => {
             x: "Week 4",
             y: 5,
           },
-          {x: "Week 1",
-          y: 2
-        },
+          { x: "Week 1", y: 2 },
         ],
       },
       {
@@ -194,8 +132,6 @@ export const HeatMapChart = () => {
             x: "Week 1",
             y: 2,
           },
-          
-        
         ],
       },
       {
@@ -219,12 +155,11 @@ export const HeatMapChart = () => {
           },
         ],
       },
-      
     ],
     options: {
       chart: {
         height: 150,
-        width:200,
+        width: 200,
         type: "heatmap",
       },
       dataLabels: {
@@ -244,12 +179,20 @@ export const HeatMapChart = () => {
     <div className="heatMapChart" id="chart">
       <ReactApexChart
         options={state.options}
-        series={state?.series}
+        series={result&&result}
         type="heatmap"
         height={350}
       />
       <div>
-        <select name="" id="" onChange={(e) => setMonth(e.target.value)}>
+        <select
+          name=""
+          id=""
+          onChange={(e) =>
+            setSelectionOptions((prev) => {
+              return { ...prev, month: e.target.value };
+            })
+          }
+        >
           <option value="January">Januray</option>
           <option value="February">February</option>
           <option value="March">March</option>

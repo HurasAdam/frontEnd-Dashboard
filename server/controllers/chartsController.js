5dbcc01567f9537de5bfe7abdf5427acd8996bae
HurasAdam <huradam@wp.pl>
2023-12-29 06:01:22 +0100

    Refactor: Enhance code readability and simplify conditional logic in getMemberContributions controller function.

--- server/controllers/chartsController.js
+++ server/controllers/chartsController.js
@@ -2,61 +2,74 @@ const userProjectActivity = require("../db/models/userProjectActivity");
 
 const getMemberContributions = async (req, res) => {
   const { id } = req.params;
-  const { contributor,month } = req.query;
+  const { contributor, month } = req.query;
+  let activity;
 
- let activity;
+console.log(req.query)
 
+  if (!contributor) {
+    const monthMap = {
+      january: 0,
+      february: 1,
+      march: 2,
+      april: 3,
+      may: 4,
+      june: 5,
+      july: 6,
+      august: 7,
+      september: 8,
+      october: 9,
+      november: 10,
+      december: 11,
+    };
+    const monthNumber = monthMap[month.toLowerCase()];
 
-if(month){
-  const monthMap = {
-    january: 0,
-    february: 1,
-    march: 2,
-    april: 3,
-    may: 4,
-    june: 5,
-    july: 6,
-    august: 7,
-    september: 8,
-    october: 9,
-    november: 10,
-    december: 11,
-  };
-  const monthNumber = monthMap[month.toLowerCase()]
- 
-  if (monthNumber !== undefined){
-    const year = new Date().getFullYear()
+    const year = new Date().getFullYear();
     startDate = new Date(year, monthNumber, 1, 1, 0, 0, 0);
     endDate = new Date(year, monthNumber + 1, 0, 23, 59, 59, 999);
-    console.log(startDate)
-    console.log(endDate)
- 
+
     activity = await userProjectActivity.find({
-      userId:contributor,
+      projectId: id,
       date: {
         $gte: startDate,
         $lt: endDate,
       },
     });
-
+   return res.status(200).json(activity);
   }
- 
-}
 
-  // const startDate = new Date('2023-12-20T00:00:00.000Z');
-  // const endDate = new Date('2023-12-22T23:59:59.999Z');
+  else if (contributor) {
+    const monthMap = {
+      january: 0,
+      february: 1,
+      march: 2,
+      april: 3,
+      may: 4,
+      june: 5,
+      july: 6,
+      august: 7,
+      september: 8,
+      october: 9,
+      november: 10,
+      december: 11,
+    };
+    const monthNumber = monthMap[month.toLowerCase()];
 
-  res.status(200).json(activity);
+    const year = new Date().getFullYear();
+    startDate = new Date(year, monthNumber, 1, 1, 0, 0, 0);
+    endDate = new Date(year, monthNumber + 1, 0, 23, 59, 59, 999);
 
-  // if (!contributor) {
-  //   activity = await userProjectActivity.find({ projectId: id });
-
-
-
-
- 
+    activity = await userProjectActivity.find({
+      projectId: id,
+      userId: contributor,
+      date: {
+        $gte: startDate,
+        $lt: endDate,
+      },
+    });
+    return res.status(200).json(activity);
+  }
 };
-
 module.exports = {
   getMemberContributions,
 };

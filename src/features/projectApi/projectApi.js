de28cdef6f13246876e3f6ac46bc59954b249388
HurasAdam <huradam@wp.pl>
2023-12-29 06:00:08 +0100

    Refactor: Simplify URL construction in projectApi getProjectActivity function.

--- src/features/projectApi/projectApi.js
+++ src/features/projectApi/projectApi.js
@@ -1,18 +1,16 @@
-import projectApi from "../axios/axios"
+import projectApi from "../axios/axios";
+import { handleCurrentMonth } from "../../utils/handleCurrentMonth";
 
+export const createProject = (projectData) => {
+  return new Promise((resolve, reject) => {
+    const response = projectApi
+      .post("projects", projectData)
+      .then((res) => resolve(res.data))
+      .catch((error) => reject(error));
+  });
+};
 
-
-export const createProject= (projectData)=>{
-
-  return new Promise((resolve,reject)=>{
-    const response = projectApi.post("projects",projectData)
-    .then((res)=>resolve(res.data))
-    .catch((error)=>reject(error))
-  })
-}
-
-
-export const getProjectList = (page,selectedSize) => {
+export const getProjectList = (page, selectedSize) => {
   return new Promise((resolve, reject) => {
     const response = projectApi
       .get(`projects?page=${page}&pageSize=${selectedSize}`)
@@ -20,18 +18,16 @@ export const getProjectList = (page,selectedSize) => {
   });
 };
 
+export const getProjectListByMembership = async () => {
+  const response = await projectApi.get("projects?membership=true");
+  return response.data;
+};
 
-export const getProjectListByMembership=async()=>{
-  const response = await projectApi.get("projects?membership=true")
-  return response.data
-}
+export const getProject = ({ projectId, query }) => {
+  let url = `projects/${projectId}`;
 
-
-export const getProject = ({projectId,query}) => {
-  let url = `projects/${projectId}`
-
-  if(query){
-    url = url+`?${query}`
+  if (query) {
+    url = url + `?${query}`;
   }
   return new Promise((resolve, reject) => {
     const response = projectApi
@@ -41,33 +37,29 @@ export const getProject = ({projectId,query}) => {
   });
 };
 
-export const updateProject = ({id,update}) => {
+export const updateProject = ({ id, update }) => {
   return new Promise((resolve, reject) => {
-console.log(`ID:${id}`)
-console.log(update)
-    const response = projectApi.patch(`projects/${id}`,update)
-   .then((res)=>resolve(res.data))
-    .catch((error)=>reject(error));
+    console.log(`ID:${id}`);
+    console.log(update);
+    const response = projectApi
+      .patch(`projects/${id}`, update)
+      .then((res) => resolve(res.data))
+      .catch((error) => reject(error));
   });
 };
 
+export const getProjectActivity = async (projectId, query) => {
 
-export const getProjectActivity= async(projectId,query)=>{
 
 
-let url = `chartData/${projectId}`
 
-if(query){
-  url = url+`?${query}`
-}
+  const {currentMonthName } = handleCurrentMonth();
+  const url = query ? `chartData/${projectId}?${query}` : `chartData/${projectId}`;
 
-  const response = await projectApi.get(url)
-  return response.data
-}
-export const deleteProject = async(id) => {
-
-  const response = await projectApi.delete(`projects/${id}`)
-  return response.data
+  const response = await projectApi.get(url);
+  return response.data;
 };
-
-
+export const deleteProject = async (id) => {
+  const response = await projectApi.delete(`projects/${id}`);
+  return response.data;
+};
